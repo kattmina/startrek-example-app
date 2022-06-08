5b3488cb9b53ac7c5c0a53660c4dcb681f3c7803
Svedin <kawi@gft.com>
2022-06-08 14:51:18 +0200

    creating and storing user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,62 +1,79 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { User } from './user.model';
 
 
 
 export interface AuthResponseData {
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
-    registered?: boolean;
+	idToken: string;
+	email: string;
+	refreshToken: string;
+	expiresIn: string;
+	localId: string;
+	registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+	user = new Subject<User>();
 
-    constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-    signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyoViNXWVrkmBPfHSYqWuZdAv7g9j6k8A',
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        ).pipe(catchError(this.handleError)
-        );
-    }
+	signup(email: string, password: string) {
+		return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyoViNXWVrkmBPfHSYqWuZdAv7g9j6k8A',
+			{
+				email: email,
+				password: password,
+				returnSecureToken: true
+			}
+		).pipe(catchError(this.handleError),
+			tap(resData => {
+				this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+			})
+		);
+	}
 
-    login(email: string, password: string) {
-        return this.http.post<AuthResponseData>(' https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAyoViNXWVrkmBPfHSYqWuZdAv7g9j6k8A',
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        ).pipe(catchError(this.handleError));
-    }
+	login(email: string, password: string) {
+		return this.http.post<AuthResponseData>(' https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAyoViNXWVrkmBPfHSYqWuZdAv7g9j6k8A',
+			{
+				email: email,
+				password: password,
+				returnSecureToken: true
+			}
+		).pipe(catchError(this.handleError));
+	}
 
-    private handleError(errorRes: HttpErrorResponse) {
-        let errorMessage = 'An unknown error occured!';
-        if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-        }
-        switch (errorRes.error.error.message) {
-            case ('EMAIL_EXISTS'):
-                errorMessage = 'This e-mail exists already.';
-                break;
-            case ('EMAIL_NOT_FOUND'):
-                errorMessage = 'This email does not exist.';
-                break;
-            case ('INVALID_PASSWORD'):
-                errorMessage = 'Incorrect password.';
-                break;
-        }
-        return throwError(errorMessage);
-    }
+	private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+		const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+		const user = new User(
+			email,
+			userId,
+			token,
+			expirationDate
+		);
+		this.user.next(user);
+	}
+
+
+	private handleError(errorRes: HttpErrorResponse) {
+		let errorMessage = 'An unknown error occured!';
+		if (!errorRes.error || !errorRes.error.error) {
+			return throwError(errorMessage);
+		}
+		switch (errorRes.error.error.message) {
+			case ('EMAIL_EXISTS'):
+				errorMessage = 'This e-mail exists already.';
+				break;
+			case ('EMAIL_NOT_FOUND'):
+				errorMessage = 'This email does not exist.';
+				break;
+			case ('INVALID_PASSWORD'):
+				errorMessage = 'Incorrect password.';
+				break;
+		}
+		return throwError(errorMessage);
+	}
 
 }
