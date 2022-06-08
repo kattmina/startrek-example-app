c5b8a8e2327be7ba047ea28d7263e5ab0df7867f
Svedin <kawi@gft.com>
2022-06-08 14:14:58 +0200

    configuration, firebase, initial components

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+
+
+export interface AuthResponseData {
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+    constructor(private http: HttpClient) { }
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyoViNXWVrkmBPfHSYqWuZdAv7g9j6k8A',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError)
+        );
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(' https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAyoViNXWVrkmBPfHSYqWuZdAv7g9j6k8A',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occured!';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case ('EMAIL_EXISTS'):
+                errorMessage = 'This e-mail exists already.';
+                break;
+            case ('EMAIL_NOT_FOUND'):
+                errorMessage = 'This email does not exist.';
+                break;
+            case ('INVALID_PASSWORD'):
+                errorMessage = 'Incorrect password.';
+                break;
+        }
+        return throwError(errorMessage);
+    }
+
+}
